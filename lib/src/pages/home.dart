47e8f493d84ad0fb1b9fcb11716380af070b5342
aidan <aidanahram1@gmail.com>
2024-07-10 19:03:43 -0400

    added some stuff

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:ebay/app.dart';
 import 'package:ebay/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:ebay/pages.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -12,17 +13,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final redirectUrl = "https://webhook.site/492b6fc0-eb70-4d3e-bfb4-a9c23409f82e";
+  final appKey = "508156";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: const Text("Ebay Drop Shipping Tool"),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              margin: EdgeInsets.zero,
+              padding: EdgeInsets.zero,
+              child: Row(
+                children: [
+                  Builder(
+                    builder: (context) {
+                      return IconButton(
+                        icon: const Icon(Icons.menu),
+                        onPressed: () {
+                          Scaffold.of(context).closeDrawer();
+                        },
+                      );
+                    },
+                  ),
+                  const Text("Menu"),
+                ],
+              )
+            ),
+            ListTile(
+              title: const Text("Authenticate Ali Express Account"),
+              onTap: () async {
+                final url = Uri.parse("https://api-sg.aliexpress.com/oauth/authorize?response_type=code&force_auth=true&redirect_uri=$redirectUrl&client_id=$appKey");
+                if(await canLaunchUrl(url)){
+                  await launchUrl(url);
+                } else {
+                  throw "Could not launch $url";
+                }
+              }
+            )
+          ]
+        )
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            const SizedBox(height: 10),
             SearchBar(
               hintText: "Search for a product",
               padding: const MaterialStatePropertyAll<EdgeInsets>(EdgeInsets.symmetric(horizontal: 16.0)),
--- lib/src/services/ali_scraper.dart
+++ lib/src/services/ali_scraper.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:io';
 
+//import 'package:flutter/services.dart';
 import 'package:requests/requests.dart';
 import 'package:crypto/crypto.dart';
 
@@ -21,12 +23,16 @@ class AliScraper {
   }
 }
 
-void main() async{
+void main() async {
   const callbackUrl = "https%3A%2F%2Fwebhook.site%2F492b6fc0-eb70-4d3e-bfb4-a9c23409f82e";
   const appKey = "508156";
   const baseUrl = "https://api-sg.aliexpress.com";
   const createTokenApi = "/auth/token/create";
-  const code = "";
+  const code = "3_508156_8VbpoXsK6RfiPK3MCSmP2GpZ32376";
+  final response = await File("C:\\Scripts\\Flutter\\Ebay\\ebay\\secrets.json").readAsString();
+  final data = await jsonDecode(response);
+  final appSecret = data["appSecret"];
+  print(appSecret);
   
 
   // final response = await Requests.post("${env["baseUrl"]}/oauth/authorize?response_type=code&force_auth=true&redirect_uri${env["callbackUrl"]}&client_id=${env["appKey"]}");
@@ -35,12 +41,11 @@ void main() async{
   final currentTime = DateTime.now().millisecondsSinceEpoch;
   print(currentTime);
 
+  final key = utf8.encode(appSecret); /// SECRET KEY
+  // const testString = "/auth/token/createapp_key12345678code3_500102_JxZ05Ux3cnnSSUm6dCxYg6Q26sign_methodsha256timestamp1517820392000";
   final stringToHash = "${createTokenApi}app_key${appKey}code${code}sign_methodsha256timestamp${currentTime.toString()}";
   print(stringToHash);
-
-  final key = utf8.encode("helloworld"); /// SECRET KEY
-  const testString = "/auth/token/createapp_key12345678code3_500102_JxZ05Ux3cnnSSUm6dCxYg6Q26sign_methodsha256timestamp1517820392000";
-  final bytes = utf8.encode(testString);
+  final bytes = utf8.encode(stringToHash);
 
   final hmacSha256 = Hmac(sha256, key);
   final digest = hmacSha256.convert(bytes);
