da42a2f07c054ecd388c37fa0e2cc8b866628cf8
aidan <aidanahram1@gmail.com>
2024-07-09 23:38:07 -0400

    nothing done tbh

--- lib/src/services/ali_scraper.dart
+++ lib/src/services/ali_scraper.dart
@@ -17,3 +17,16 @@ class AliScraper {
     print("Scraping page ${endpoint}");
   }
 }
+
+void main() async{
+  final env = {
+    "callbackUrl": "https%3A%2F%2Fwebhook.site%2F492b6fc0-eb70-4d3e-bfb4-a9c23409f82e",
+    "appKey": "508156",
+    "baseUrl": "https://api-sg.aliexpress.com",
+  };
+
+  final response = await Requests.post("${env["baseUrl"]}/oauth/authorize?response_type=code&force_auth=true&redirect_uri${env["callbackUrl"]}&client_id=${env["appKey"]}");
+  response.throwForStatus();
+
+
+}
