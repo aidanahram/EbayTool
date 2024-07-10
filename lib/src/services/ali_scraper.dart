534f0fd9eac3bb838222f37115794c68ac11548a
aidan <aidanahram1@gmail.com>
2024-07-09 23:58:05 -0400

    started sha

--- lib/src/services/ali_scraper.dart
+++ lib/src/services/ali_scraper.dart
@@ -1,4 +1,5 @@
 import 'package:requests/requests.dart';
+import 'package:crypto/crypto.dart';
 
 class AliScraper {
   const AliScraper();
@@ -19,14 +20,22 @@ class AliScraper {
 }
 
 void main() async{
-  final env = {
-    "callbackUrl": "https%3A%2F%2Fwebhook.site%2F492b6fc0-eb70-4d3e-bfb4-a9c23409f82e",
-    "appKey": "508156",
-    "baseUrl": "https://api-sg.aliexpress.com",
-  };
+  const callbackUrl = "https%3A%2F%2Fwebhook.site%2F492b6fc0-eb70-4d3e-bfb4-a9c23409f82e";
+  const appKey = "508156";
+  const baseUrl = "https://api-sg.aliexpress.com";
+  const createTokenApi = "/auth/token/create";
+  const code = "";
+  
 
-  final response = await Requests.post("${env["baseUrl"]}/oauth/authorize?response_type=code&force_auth=true&redirect_uri${env["callbackUrl"]}&client_id=${env["appKey"]}");
-  response.throwForStatus();
+  // final response = await Requests.post("${env["baseUrl"]}/oauth/authorize?response_type=code&force_auth=true&redirect_uri${env["callbackUrl"]}&client_id=${env["appKey"]}");
+  // response.throwForStatus();
+
+  final currentTime = DateTime.now().millisecondsSinceEpoch;
+  print(currentTime);
+
+  final stringToHash = "${createTokenApi}app_key${appKey}code${code}sign_methodsha256timestamp${currentTime.toString()}";
+  print(stringToHash);
+
 
 
 }
