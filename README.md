52cdc31c8d5510221d35eba6cc5413fa9bf98543
aidan <aidanahram1@gmail.com>
2024-07-13 15:38:05 -0400

    generates ali express api keys

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
 # ebay
 
-A new Flutter project.
+A flutter project to manage dropshipping
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+Download and run
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## TODO
+Maybe make dart ali express sdk
--- lib/src/services/ali_scraper.dart
+++ lib/src/services/ali_scraper.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'dart:convert';
 import 'dart:io';
 
@@ -20,51 +22,46 @@ class AliScraper {
   }
 
   void scrapePage(String endpoint){
-    print("Scraping page ${endpoint}");
+    print("Scraping page $endpoint");
   }
 }
 
 void main() async {
   const callbackUrl = "https%3A%2F%2Fwebhook.site%2F492b6fc0-eb70-4d3e-bfb4-a9c23409f82e";
-  const baseUrl = "https://api-sg.aliexpress.com";
+  const baseUrl = "https://api-sg.aliexpress.com/rest";
   const createTokenApi = "/auth/token/create";
-  final json = await File("C:\\Scripts\\Flutter\\Ebay\\ebay\\secrets.json").readAsString();
-  final data = await jsonDecode(json);
+  final secrets = await File("C:\\Scripts\\Flutter\\Ebay\\ebay\\secrets.json").readAsString();
+  final data = await jsonDecode(secrets);
   final appSecret = data["appSecret"];
   print(appSecret);
   
   final currentTime = DateTime.now().millisecondsSinceEpoch;
-  print(currentTime);
   final params = {
     "app_key": "508156",
-    "code": "3_508156_OeEzy3orBcoG04X1B6GZ3uOp172",
+    "code": "3_508156_kRZ1x2UdXo7697q67eWOoyvo436",
     "sign_method": "sha256",
-    "timestamp": currentTime,
+    "timestamp": currentTime.toString(),
   };
-  // final response = await Requests.post("${env["baseUrl"]}/oauth/authorize?response_type=code&force_auth=true&redirect_uri${env["callbackUrl"]}&client_id=${env["appKey"]}");
-  // response.throwForStatus();
 
   final key = utf8.encode(appSecret); /// SECRET KEY
   // const testString = "/auth/token/createapp_key12345678code3_500102_JxZ05Ux3cnnSSUm6dCxYg6Q26sign_methodsha256timestamp1517820392000";
-  final stringToHash = "${createTokenApi}app_key${params["appKey"]}code${params["code"]}sign_methodsha256timestamp${currentTime.toString()}";
+  final stringToHash = "${createTokenApi}app_key${params["app_key"]}code${params["code"]}sign_methodsha256timestamp${params["timestamp"]}";
   print(stringToHash);
   final bytes = utf8.encode(stringToHash);
 
   final hmacSha256 = Hmac(sha256, key);
   final digest = hmacSha256.convert(bytes);
 
-  print("Digest as bytes: ${digest.bytes}");
   print("Digest as hex string: $digest");
-  params["sign"] = digest.toString();
+  params["sign"] = digest.toString().toUpperCase();
 
   String url = "$baseUrl$createTokenApi?";
-  params.forEach((key, value) {url += "&$key=$value"; });
+
   print(url);
-  final response = await Requests.get(url);
+  print("\n\n\n");
+  final response = await Requests.get(url, queryParameters: params, withCredentials: true);
   print(response.body);
-
-  // final bytes = utf8.encode(stringToHash);
-
-  // final digest = sha256.convert(bytes);
-
+  final json = jsonDecode(response.body);
+  print(json.toString());
+  print(DateTime.fromMicrosecondsSinceEpoch(1723490893000).toLocal());
 }
