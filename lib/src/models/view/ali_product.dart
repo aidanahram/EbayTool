c5eeb85b695c6c2b84daa7f57210056ae27893f3
aidan <aidanahram1@gmail.com>
2024-07-24 23:00:48 -0400

    its coming along but its taking a long time

--- lib/src/models/view/ali_product.dart
+++ lib/src/models/view/ali_product.dart
@@ -4,11 +4,20 @@ import "dart:ui";
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
 
+class SubItem {
+  final String name;
+  final String price;
+  final String? imageLink;
+  SubItem(this.name, this.price, {this.imageLink});
+}
+
 class AliProductModel with ChangeNotifier {
   String response = "";
   dynamic data;
-  String itemName = "";
+  String productName = "";
+  int? price;
   List<String> imageLinks = [];
+  List<SubItem> items = [];
   int mainIndex = 0;
 
   AliProductModel();
@@ -17,7 +26,8 @@ class AliProductModel with ChangeNotifier {
     try{
       data = json.decode(response);
       imageLinks = _getProductImages(data);
-      itemName = data['aliexpress_ds_product_get_response']['result']['ae_item_base_info_dto']['subject'];
+      items = _getSubItems(data);
+      productName = data['aliexpress_ds_product_get_response']['result']['ae_item_base_info_dto']['subject'];
       notifyListeners();
     } on Exception catch (e) {
       print("Error was thrown while trying to initialize");
@@ -30,8 +40,27 @@ class AliProductModel with ChangeNotifier {
     return urls.toString().split(';');
   }
 
+  List<SubItem>  _getSubItems(dynamic data){
+    List<SubItem> res = [];
+    
+    final items = data['aliexpress_ds_product_get_response']['result']['ae_item_sku_info_dtos']['ae_item_sku_info_d_t_o'];
+    for(final item in items){
+      final name = item['ae_sku_property_dtos']['ae_sku_property_d_t_o'][0]['property_value_definition_name'];
+      final price = item['offer_sale_price'];
+      if(item['ae_sku_property_dtos']['ae_sku_property_d_t_o'][0].containsKey('sku_image')){
+        final imageLink = item['ae_sku_property_dtos']['ae_sku_property_d_t_o'][0]['sku_image'];
+        res.add(SubItem(name, price, imageLink: imageLink));
+      } else {
+        res.add(SubItem(name, price));
+      }
+    }
+    return res;
+  }
+
   void setMainImage(int index){
     mainIndex = index;
     notifyListeners();
   }
+
+
 }
--- lib/src/pages/add_item.dart
+++ lib/src/pages/add_item.dart
@@ -44,14 +44,14 @@ class AddItemPage extends ReactiveWidget<AliProductModel> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(model.itemName,
+                    Text(model.productName,
                       style: const TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
                       )
                     ),
                     const Text("Images of Product"),
-                    Image.network(model.imageLinks[model.mainIndex], width: 500, height: 500),
+                    Image.network(model.imageLinks[model.mainIndex], width: 450, height: 450),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [for(int i = 0; i < model.imageLinks.length; i++) Material(
@@ -59,10 +59,27 @@ class AddItemPage extends ReactiveWidget<AliProductModel> {
                           onTap: () { model.setMainImage(i); },
                           child: Image.network(
                             model.imageLinks[i], width: 150, height: 150,
-                          )
-                        )
-                      )]
-                    ),  
+                          ),
+                        ),
+                      ),],
+                    ),
+                    const SizedBox(height: 30.0,),
+                    const Text("Items Found on Page. Select one and choose which images to keep. Beware of duplicates",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10.0,),
+                    Row(
+                      children: [for(int i = 0; i < model.items.length; i++) Column(children: [
+                        Text(model.items[i].name),
+                        model.items[i].imageLink != null ? Image.network(model.items[i].imageLink!, width: 200, height: 200,) : const SizedBox(width: 200, height: 200),
+                        Text(model.items[i].price.toString()),
+                      ],),
+                      const SizedBox(width: 10.0,),
+                      ],
+                    ),
                   ],
                 ),
               ),
